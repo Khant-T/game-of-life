6f2640dea197b73c91300a96537fa7eace1afca4
Khant Tayza <khant.tz@proton.me>
2023-02-15 19:14:44 -0600

    testing SDL2

--- main.cpp
+++ main.cpp
@@ -3,9 +3,54 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char*[])
 {
-	cout << "Hello, world!";
-	SDL_Init(SDL_INIT_VIDEO);
+	cout << "Program started." << endl;
+
+	SDL_Window* myWindow;
+	myWindow = SDL_CreateWindow(
+		"Hello, world!",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800,
+		800,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+	);
+
+	SDL_Renderer* myRenderer;
+	myRenderer = SDL_CreateRenderer(
+		myWindow,
+		-1,
+		SDL_RENDERER_PRESENTVSYNC
+	);
+
+
+	bool running = true;
+	while (running)
+	{
+		SDL_Event ev;
+		SDL_WaitEvent(&ev);
+
+		if (ev.type == SDL_WINDOWEVENT)
+		{
+			if (ev.window.event == SDL_WINDOWEVENT_CLOSE)
+				running = false;
+		}
+
+		if (ev.type == SDL_KEYDOWN)
+		{
+			if (ev.key.keysym.sym == SDLK_ESCAPE)
+				running = false;
+		}
+
+		SDL_SetRenderDrawColor(myRenderer, 100, 100, 100, 255);
+		SDL_RenderClear(myRenderer);
+
+		SDL_RenderPresent(myRenderer);
+	}
+
+	cout << "Program closed." << endl;
+	SDL_DestroyRenderer(myRenderer);
+	SDL_DestroyWindow(myWindow);
 	return 0;
 }
